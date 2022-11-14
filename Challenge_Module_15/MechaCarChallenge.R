c7343baa13bb6c6a3fa05bfc212b715b8eedd967
elina dart <dartelina4@gmail.com>
2022-11-14 11:27:05 -0500

    additional analysis in D1

--- Challenge_Module_15/MechaCarChallenge.R
+++ Challenge_Module_15/MechaCarChallenge.R
@@ -3,7 +3,10 @@ library(dplyr)
 MechaCarMPG <- read.csv("MechaCar_mpg.csv", header= TRUE,sep = ",", stringsAsFactors = F)
 lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=MechaCarMPG)
 summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=MechaCarMPG))
-
+# mpg= 104 + 6.267VL + .0012VW + .069SA + 3.546GC - 3.411AWD 
+# i'm concerned that independent variables are correlated and could effect the results so i'm runnong a cor() function on the 2 variables that I believe could be corelated.  
+cor(MechaCarMPG$vehicle_length, MechaCarMPG$vehicle_weight)
+# variables are not correlated 
 # DELIVERABLE 2 - Provide Summary Statistics for Suspension Coil PSI
 Suspension_data <- read.csv("Suspension_Coil.csv", header = T, sep = ",", stringsAsFactors = F)
 total_summary <- Suspension_data %>% summarize(mean(PSI), median(PSI), var(PSI), sd(PSI), .groups = 'keep')
@@ -13,7 +16,7 @@ lot_summary <- Suspension_data %>% group_by(Manufacturing_Lot) %>% summarize(mea
 # H0= PSI for each Manufacturing Lot is the same the population Mean (1500)
 # Ha= PSI is statistical different from the population mean (1500)
 t.test(Suspension_data$PSI, mu=1500)
-
+# Test each Lot
 t.test(subset(Suspension_data$PSI, Suspension_data$Manufacturing_Lot=='Lot1'), mu=1500)
 t.test(subset(Suspension_data$PSI, Suspension_data$Manufacturing_Lot=='Lot2'), mu=1500)
 t.test(subset(Suspension_data$PSI, Suspension_data$Manufacturing_Lot=='Lot3'), mu=1500)
